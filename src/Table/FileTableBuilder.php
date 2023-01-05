3c8826500096af571045c0b3280d5128db95a37f
Claus - Kalna IT <claus@kalna.it>
2023-01-05 16:00:27 +0100

    Restyle file selector and changed default sorting with the option to change it in preferences

--- src/Table/FileTableBuilder.php
+++ src/Table/FileTableBuilder.php
@@ -2,6 +2,8 @@
 
 use Anomaly\FilesModule\File\FileModel;
 use Anomaly\FilesModule\Folder\Command\GetFolder;
+use Anomaly\PreferencesModule\Preference\Contract\PreferenceRepositoryInterface;
+use Anomaly\Streams\Platform\Ui\Table\Table;
 use Anomaly\Streams\Platform\Ui\Table\TableBuilder;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -14,6 +16,14 @@ use Illuminate\Database\Eloquent\Builder;
  */
 class FileTableBuilder extends TableBuilder
 {
+    public function __construct(PreferenceRepositoryInterface $preferences, Table $table)
+    {
+        parent::__construct($table);
+        $default_order_by = $preferences->value('anomaly.field_type.file::default_order_by', 'updated_at');
+        $default_order_direction = $preferences->value('anomaly.field_type.file::default_order_direction', 'desc');
+
+        $this->options['order_by'] = [$default_order_by => $default_order_direction];
+    }
 
     /**
      * Field configuration.
@@ -45,28 +55,36 @@ class FileTableBuilder extends TableBuilder
         'entry.preview' => [
             'heading' => 'anomaly.module.files::field.preview.name',
         ],
-        'name'          => [
+        'name' => [
             'sort_column' => 'name',
-            'wrapper'     => '
+            'wrapper' => '
                     <strong>{value.file}</strong>
                     <br>
                     <small class="text-muted">{value.disk}://{value.folder}/{value.file}</small>
                     <br>
-                    <span>{value.size} {value.keywords}</span>',
-            'value'       => [
-                'file'     => 'entry.name',
-                'folder'   => 'entry.folder.slug',
+                    <span><span class="tag {value.size_tag_type} tag-sm">{value.readable_file_size}</span> <span class="tag tag-default tag-sm text-gray-dark">{value.mime_type}</span> {value.size} {value.keywords}</span>',
+            'value' => [
+                'file' => 'entry.name',
+                'mime_type' => 'entry.mime_type',
+                'folder' => 'entry.folder.slug',
                 'keywords' => 'entry.keywords.labels|join',
-                'disk'     => 'entry.folder.disk.slug',
-                'size'     => 'entry.size_label',
+                'disk' => 'entry.folder.disk.slug',
+                'size' => 'entry.size_label',
+                'size_tag_type' => 'entry.size.value < 150000 ? "tag-info" : entry.size.value < 350000 ? "tag-warning" : "tag-danger"',
+                'readable_file_size' => 'entry.readable_size',
             ],
         ],
-        'size'          => [
-            'sort_column' => 'size',
-            'value'       => 'entry.readable_size',
-        ],
-        'mime_type',
         'folder',
+        'updated_at' => [
+            'sort_column' => 'updated_at',
+            'wrapper' => '{value.updated_at_human}
+                              <br>
+                              <small class="text-muted">{value.updated_at}</small>',
+            'value' => [
+                'updated_at' => 'entry.updated_at.format("M jS, Y, H:i")',
+                'updated_at_human' => 'entry.updated_at.diffForHumans()'
+            ],
+        ],
     ];
 
     /**
@@ -87,7 +105,7 @@ class FileTableBuilder extends TableBuilder
      */
     protected $options = [
         'enable_views' => false,
-        'title'        => 'anomaly.field_type.file::message.choose_file',
+        'title' => 'anomaly.field_type.file::message.choose_file',
     ];
 
     /**
@@ -138,7 +156,7 @@ class FileTableBuilder extends TableBuilder
     /**
      * Set the config.
      *
-     * @param  array $config
+     * @param array $config
      * @return $this
      */
     public function setConfig(array $config = [])
